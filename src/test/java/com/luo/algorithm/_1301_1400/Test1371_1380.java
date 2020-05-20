63f617775a8f160055aa240d0100f9aaade12292
罗远新 <402135175@qq.com>
2020-05-20 09:30:17 +0800

    leetCode 1371

---
+++ src/test/java/com/luo/algorithm/_1301_1400/Test1371_1380.java
@@ -0,0 +1,13 @@
+package com.luo.algorithm._1301_1400;
+
+import org.junit.Test;
+
+public class Test1371_1380 {
+    T1371_1380 t = new T1371_1380();
+    @Test
+    public void test1371(){
+        System.out.println(t.findTheLongestSubstring("eleetminicoworoep"));
+        System.out.println(t.findTheLongestSubstring("leetcodeisgreat"));
+        System.out.println(t.findTheLongestSubstring("bcbcbc"));
+    }
+}
