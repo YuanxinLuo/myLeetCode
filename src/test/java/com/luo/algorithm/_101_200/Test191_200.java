8f3a65e59956af6f400b34195dcd0d429c43e0d8
罗远新 <402135175@qq.com>
2020-05-29 10:10:52 +0800

    算法198

---
+++ src/test/java/com/luo/algorithm/_101_200/Test191_200.java
@@ -0,0 +1,14 @@
+package com.luo.algorithm._101_200;
+
+import org.junit.Test;
+
+public class Test191_200 {
+    T191_200 t = new T191_200();
+
+    @Test
+    public void test198() {
+        System.out.println(t.rob(new int[]{1, 2, 3, 1}));
+        System.out.println(t.rob(new int[]{2, 7, 9, 3, 1}));
+        System.out.println(t.rob(new int[]{2, 1, 1, 2}));
+    }
+}
