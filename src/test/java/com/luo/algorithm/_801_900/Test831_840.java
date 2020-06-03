9d2f8489a6fb913184f5a8dc861df31b6e40f50b
罗远新 <402135175@qq.com>
2020-06-03 09:37:04 +0800

    算法837

---
+++ src/test/java/com/luo/algorithm/_801_900/Test831_840.java
@@ -0,0 +1,14 @@
+package com.luo.algorithm._801_900;
+
+import org.junit.Test;
+
+public class Test831_840 {
+    T831_840 t = new T831_840();
+
+    @Test
+    public void test() {
+        System.out.println(t.new21Game(10, 1, 10));
+        System.out.println(t.new21Game(6, 1, 10));
+        System.out.println(t.new21Game(21, 17, 10));
+    }
+}
