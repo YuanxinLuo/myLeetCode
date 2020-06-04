7d41f5837f93dc323e5a4ff473fd8811659cc31d
罗远新 <402135175@qq.com>
2020-06-04 09:41:01 +0800

    算法238

--- src/main/java/com/luo/algorithm/_201_300/T231_240.java
+++ src/main/java/com/luo/algorithm/_201_300/T231_240.java
@@ -44,11 +44,37 @@ public class T231_240 {
 ////            return right;
 ////        }
 ////        return null;
-        if(left == null){
+        if (left == null) {
             return right;
-        }else if(right ==null){
+        } else if (right == null) {
             return left;
         }
         return root;
+    }
+
+    /**
+     * 238. 除自身以外数组的乘积
+     * 给你一个长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，其中 output[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积。
+     * 示例:
+     * 输入: [1,2,3,4]
+     * 输出: [24,12,8,6]
+     * 提示：题目数据保证数组之中任意元素的全部前缀元素和后缀（甚至是整个数组）的乘积都在 32 位整数范围内。
+     * 说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+     *
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        int suffix = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            suffix = suffix * nums[i + 1];
+            result[i] *= suffix;
+        }
+        return result;
     }
 }
--- src/test/java/com/luo/algorithm/_201_300/Test231_240.java
+++ src/test/java/com/luo/algorithm/_201_300/Test231_240.java
@@ -1,7 +1,19 @@
 package com.luo.algorithm._201_300;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class Test231_240 {
     T231_240 t = new T231_240();
 
+    @Test
+    public void test236() {
 
+    }
+
+    @Test
+    public void test238() {
+        System.out.println(Arrays.toString(t.productExceptSelf(new int[]{1, 2, 3, 4})));
+    }
 }
