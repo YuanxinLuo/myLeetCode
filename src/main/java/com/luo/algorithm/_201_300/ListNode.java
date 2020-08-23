861bdaf21a4a57ee501042c2eaad01a8e87d6b1f
YuanXinLuo <402135175@qq.com>
2020-08-23 22:13:12 +0800

    201,202,203

---
+++ src/main/java/com/luo/algorithm/_201_300/ListNode.java
@@ -0,0 +1,18 @@
+package com.luo.algorithm._201_300;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
--- src/test/java/com/luo/algorithm/_201_300/Test201_210.java
+++ src/test/java/com/luo/algorithm/_201_300/Test201_210.java
@@ -9,6 +9,28 @@ public class Test201_210 {
     T201_210 t = new T201_210();
 
     @Test
+    public void test201() {
+        System.out.println(t.rangeBitwiseAnd(5, 7));
+        System.out.println(t.rangeBitwiseAnd(0, 1));
+    }
+
+    @Test
+    public void test202() {
+        System.out.println(t.isHappy(19));
+    }
+
+    @Test
+    public void test203() {
+        ListNode head= new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println(t.removeElements(head, 6));
+    }
+
+    @Test
     public void test209() {
         System.out.println(t.minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3}));
     }
