702085a713f7545219eb89aa2086afee5f597c2b
YuanXinLuo <402135175@qq.com>
2020-08-30 21:46:28 +0800

    551,553,557

--- src/main/java/com/luo/algorithm/_501_600/T551_560.java
+++ src/main/java/com/luo/algorithm/_501_600/T551_560.java
@@ -4,6 +4,89 @@ import java.util.HashMap;
 
 public class T551_560 {
 
+    /**
+     * 551. 学生出勤记录 I
+     * 给定一个字符串来代表一个学生的出勤记录，这个记录仅包含以下三个字符：
+     * <p>
+     * 'A' : Absent，缺勤
+     * 'L' : Late，迟到
+     * 'P' : Present，到场
+     * 如果一个学生的出勤记录中不超过一个'A'(缺勤)并且不超过两个连续的'L'(迟到),那么这个学生会被奖赏。
+     * 你需要根据这个学生的出勤记录判断他是否会被奖赏。
+     * <p>
+     * 示例 1:
+     * 输入: "PPALLP"
+     * 输出: True
+     * <p>
+     * 示例 2:
+     * 输入: "PPALLL"
+     * 输出: False
+     */
+    public boolean checkRecord(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++)
+            if (s.charAt(i) == 'A')
+                count++;
+        return count < 2 && s.indexOf("LLL") < 0;
+    }
+
+    /**
+     * 553. 最优除法
+     * 给定一组正整数，相邻的整数之间将会进行浮点除法操作。例如， [2,3,4] -> 2 / 3 / 4 。
+     * <p>
+     * 但是，你可以在任意位置添加任意数目的括号，来改变算数的优先级。你需要找出怎么添加括号，才能得到最大的结果，并且返回相应的字符串格式的表达式。你的表达式不应该含有冗余的括号。
+     * <p>
+     * 示例：
+     * <p>
+     * 输入: [1000,100,10,2]
+     * 输出: "1000/(100/10/2)"
+     * 解释:
+     * 1000/(100/10/2) = 1000/((100/10)/2) = 200
+     * 但是，以下加粗的括号 "1000/((100/10)/2)" 是冗余的，
+     * 因为他们并不影响操作的优先级，所以你需要返回 "1000/(100/10/2)"。
+     * <p>
+     * 其他用例:
+     * 1000/(100/10)/2 = 50
+     * 1000/(100/(10/2)) = 50
+     * 1000/100/10/2 = 0.5
+     * 1000/100/(10/2) = 2
+     */
+    public String optimalDivision(int[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            sb.append(nums[i]);
+            sb.append("/");
+        }
+        return nums.length < 3 ? sb.deleteCharAt(sb.lastIndexOf("/")).toString() : sb.deleteCharAt(sb.lastIndexOf("/")).insert(sb.indexOf("/") + 1, "(").append(")").toString();
+    }
+
+    /**
+     * 557. 反转字符串中的单词 III
+     * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+     * 示例：
+     * <p>
+     * 输入："Let's take LeetCode contest"
+     * 输出："s'teL ekat edoCteeL tsetnoc"
+     */
+    public String reverseWords(String s) {
+        int len = s.length();
+        char[] chars = s.toCharArray();
+        char[] res = new char[len];
+        int left = 0, right = 0;
+        int ptr = 0;
+        while (left < len && right < len) {
+            while (right < len && chars[right] != ' ') {
+                right++;
+            }
+            for (int i = right - 1; i >= left; i--) {
+                res[ptr++] = chars[i];
+            }
+            if (ptr < len - 1) res[ptr++] = ' ';
+            left = right + 1;
+            right++;
+        }
+        return String.valueOf(res);
+    }
 
     /**
      * 560. 和为K的子数组
--- src/test/java/com/luo/algorithm/_501_600/Test551_560.java
+++ src/test/java/com/luo/algorithm/_501_600/Test551_560.java
@@ -6,6 +6,23 @@ public class Test551_560 {
     T551_560 t = new T551_560();
 
     @Test
+    public void test551() {
+        System.out.println(t.checkRecord("PPALLP"));
+        System.out.println(t.checkRecord("PPALLL"));
+    }
+
+
+    @Test
+    public void test553() {
+        System.out.println(t.optimalDivision(new int[]{1000, 100, 10, 2}));
+    }
+
+    @Test
+    public void test557() {
+        System.out.println(t.reverseWords("Let's take LeetCode contest"));
+    }
+
+    @Test
     public void test560() {
         int[] nums1 = {1, 1, 1};
         int k = 2;
