9c23064eec924981ec49f0a767083a3b7372e9fe
罗远新 <402135175@qq.com>
2020-09-03 09:58:34 +0800

    51,52,54,55,59

--- src/main/java/com/luo/algorithm/_1_100/T51_60.java
+++ src/main/java/com/luo/algorithm/_1_100/T51_60.java
@@ -1,6 +1,143 @@
 package com.luo.algorithm._1_100;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class T51_60 {
+    /**
+     * 51. N 皇后
+     * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+     * 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+     * 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+     * 示例：
+     * 输入：4
+     * 输出：[
+     * [".Q..",  // 解法 1
+     * "...Q",
+     * "Q...",
+     * "..Q."],
+     * <p>
+     * ["..Q.",  // 解法 2
+     * "Q...",
+     * "...Q",
+     * ".Q.."]
+     * ]
+     * 解释: 4 皇后问题存在两个不同的解法。
+     */
+    private List<List<String>> solutions;
+    private char[][] nQueens;
+    private boolean[] colUsed;
+    private boolean[] diagonals45Used;
+    private boolean[] diagonals135Used;
+    private int n;
+
+    public List<List<String>> solveNQueens(int n) {
+        solutions = new ArrayList<>();
+        nQueens = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(nQueens[i], '.');
+        }
+        colUsed = new boolean[n];
+        diagonals45Used = new boolean[2 * n - 1];
+        diagonals135Used = new boolean[2 * n - 1];
+        this.n = n;
+        backtracking(0);
+        return solutions;
+    }
+
+    private void backtracking(int row) {
+        if (row == n) {
+            List<String> list = new ArrayList<>();
+            for (char[] chars : nQueens) {
+                list.add(new String(chars));
+            }
+            solutions.add(list);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            int diagonals45Idx = row + col;
+            int diagonals135Idx = n - 1 - (row - col);
+            if (colUsed[col] || diagonals45Used[diagonals45Idx] || diagonals135Used[diagonals135Idx]) {
+                continue;
+            }
+            nQueens[row][col] = 'Q';
+            colUsed[col] = diagonals45Used[diagonals45Idx] = diagonals135Used[diagonals135Idx] = true;
+            backtracking(row + 1);
+            colUsed[col] = diagonals45Used[diagonals45Idx] = diagonals135Used[diagonals135Idx] = false;
+            nQueens[row][col] = '.';
+        }
+    }
+
+    /**
+     * 52. N皇后 II
+     * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+     * 给定一个整数 n，返回 n 皇后不同的解决方案的数量。
+     * 示例:
+     * 输入: 4
+     * 输出: 2
+     * 解释: 4 皇后问题存在如下两个不同的解法。
+     * [
+     * [".Q..",  // 解法 1
+     * "...Q",
+     * "Q...",
+     * "..Q."],
+     * <p>
+     * ["..Q.",  // 解法 2
+     * "Q...",
+     * "...Q",
+     * ".Q.."]
+     * ]
+     */
+    public int totalNQueens(int n) {
+        return backtrack(0, 0, 0, 0, 0, n);
+    }
+
+    private int backtrack(int row, int hills, int next_row, int dales, int count, int n) {
+        /**
+         row: 当前放置皇后的行号
+         hills: 主对角线占据情况 [1 = 被占据，0 = 未被占据]
+         next_row: 下一行被占据的情况 [1 = 被占据，0 = 未被占据]
+         dales: 次对角线占据情况 [1 = 被占据，0 = 未被占据]
+         count: 所有可行解的个数
+         */
+
+        // 棋盘所有的列都可放置，
+        // 即，按位表示为 n 个 '1'
+        // bin(cols) = 0b1111 (n = 4), bin(cols) = 0b111 (n = 3)
+        // [1 = 可放置]
+        int columns = (1 << n) - 1;
+
+        if (row == n)   // 如果已经放置了 n 个皇后
+            count++;  // 累加可行解
+        else {
+            // 当前行可用的列
+            // ! 表示 0 和 1 的含义对于变量 hills, next_row and dales的含义是相反的
+            // [1 = 未被占据，0 = 被占据]
+            int free_columns = columns & ~(hills | next_row | dales);
+
+            // 找到可以放置下一个皇后的列
+            while (free_columns != 0) {
+                // free_columns 的第一个为 '1' 的位
+                // 在该列我们放置当前皇后
+                int curr_column = -free_columns & free_columns;
+
+                // 放置皇后
+                // 并且排除对应的列
+                free_columns ^= curr_column;
+
+                count = backtrack(row + 1,
+                        (hills | curr_column) << 1,
+                        next_row | curr_column,
+                        (dales | curr_column) >> 1,
+                        count, n);
+            }
+        }
+
+        return count;
+    }
+
     /**
      * 53. 最大子序和
      * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
@@ -27,6 +164,89 @@ public class T51_60 {
     }
 
     /**
+     * 54. 螺旋矩阵
+     * 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入:
+     * [
+     * [ 1, 2, 3 ],
+     * [ 4, 5, 6 ],
+     * [ 7, 8, 9 ]
+     * ]
+     * 输出: [1,2,3,6,9,8,7,4,5]
+     * 示例 2:
+     * <p>
+     * 输入:
+     * [
+     * [1, 2, 3, 4],
+     * [5, 6, 7, 8],
+     * [9,10,11,12]
+     * ]
+     * 输出: [1,2,3,4,8,12,11,10,9,5,6,7]
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> order = new ArrayList<Integer>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return order;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int left = 0, right = columns - 1, top = 0, bottom = rows - 1;
+        while (left <= right && top <= bottom) {
+            for (int column = left; column <= right; column++) {
+                order.add(matrix[top][column]);
+            }
+            for (int row = top + 1; row <= bottom; row++) {
+                order.add(matrix[row][right]);
+            }
+            if (left < right && top < bottom) {
+                for (int column = right - 1; column > left; column--) {
+                    order.add(matrix[bottom][column]);
+                }
+                for (int row = bottom; row > top; row--) {
+                    order.add(matrix[row][left]);
+                }
+            }
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+        return order;
+    }
+
+    /**
+     * 55. 跳跃游戏
+     * 给定一个非负整数数组，你最初位于数组的第一个位置。
+     * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+     * 判断你是否能够到达最后一个位置。
+     * <p>
+     * 示例 1:
+     * 输入: [2,3,1,1,4]
+     * 输出: true
+     * 解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3 步到达最后一个位置。
+     * <p>
+     * 示例 2:
+     * 输入: [3,2,1,0,4]
+     * 输出: false
+     * 解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+     *
+     * @param nums
+     * @return
+     */
+    public boolean canJump(int[] nums) {
+        int n=nums.length;
+        int last=0;
+        for(int i=1;i<n;i++){
+            while(last<i&&last+nums[last]<i)
+                last++;
+            if(last==i) return false;
+        }
+        return true;
+    }
+
+    /**
      * 58. 最后一个单词的长度
      * 给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
      * 如果不存在最后一个单词，请返回 0 。
@@ -43,5 +263,35 @@ public class T51_60 {
         String[] s1 = s.split(" ");
         if (s1.length == 0) return 0;
         return s1[s1.length - 1].length();
+    }
+
+    /**
+     * 59. 螺旋矩阵 II
+     * 给定一个正整数 n，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
+     * 示例:
+     *
+     * 输入: 3
+     * 输出:
+     * [
+     *  [ 1, 2, 3 ],
+     *  [ 8, 9, 4 ],
+     *  [ 7, 6, 5 ]
+     * ]
+     */
+    public int[][] generateMatrix(int n) {
+        int l = 0, r = n - 1, t = 0, b = n - 1;
+        int[][] mat = new int[n][n];
+        int num = 1, tar = n * n;
+        while(num <= tar){
+            for(int i = l; i <= r; i++) mat[t][i] = num++; // left to right.
+            t++;
+            for(int i = t; i <= b; i++) mat[i][r] = num++; // top to bottom.
+            r--;
+            for(int i = r; i >= l; i--) mat[b][i] = num++; // right to left.
+            b--;
+            for(int i = b; i >= t; i--) mat[i][l] = num++; // bottom to top.
+            l++;
+        }
+        return mat;
     }
 }
--- src/test/java/com/luo/algorithm/_1_100/Test51_60.java
+++ src/test/java/com/luo/algorithm/_1_100/Test51_60.java
@@ -2,8 +2,20 @@ package com.luo.algorithm._1_100;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class Test51_60 {
     T51_60 t = new T51_60();
+
+    @Test
+    public void test51() {
+        System.out.println(t.solveNQueens(4));
+    }
+
+    @Test
+    public void test52() {
+        System.out.println(t.totalNQueens(4));
+    }
 
     @Test
     public void test53() {
@@ -12,8 +24,26 @@ public class Test51_60 {
     }
 
     @Test
+    public void test54() {
+        System.out.println(t.spiralOrder(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}));
+        System.out.println(t.spiralOrder(new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}));
+    }
+
+    @Test
+    public void test55() {
+        System.out.println(t.canJump(new int[]{2, 3, 1, 1, 4}));
+        System.out.println(t.canJump(new int[]{3, 2, 1, 0, 4}));
+    }
+
+    @Test
     public void test58() {
         System.out.println(t.lengthOfLastWord("Hello world"));
         System.out.println(t.lengthOfLastWord(" "));
+    }
+
+    @Test
+    public void test59(){
+        int[][] ints = t.generateMatrix(3);
+        Arrays.stream(ints).forEach(i-> System.out.println(Arrays.toString(i)));
     }
 }
