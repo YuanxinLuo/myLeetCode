e6d2e207baae93a8a4bf984442794273e6bd83e5
罗远新 <402135175@qq.com>
2020-09-14 14:50:14 +0800

    81,82,85,86,87

--- src/main/java/com/luo/algorithm/_1_100/T81_90.java
+++ src/main/java/com/luo/algorithm/_1_100/T81_90.java
@@ -6,6 +6,77 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class T81_90 {
+    /**
+     * 81. 搜索旋转排序数组 II
+     * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+     * ( 例如，数组 [0,0,1,2,2,5,6] 可能变为 [2,5,6,0,0,1,2] )。
+     * 编写一个函数来判断给定的目标值是否存在于数组中。若存在返回 true，否则返回 false。
+     * <p>
+     * 示例 1:
+     * 输入: nums = [2,5,6,0,0,1,2], target = 0
+     * 输出: true
+     * <p>
+     * 示例 2:
+     * 输入: nums = [2,5,6,0,0,1,2], target = 3
+     * 输出: false
+     */
+    public boolean search(int[] nums, int target) {
+        int len = nums.length;
+        if (len == 0) {
+            return false;
+        }
+
+        int left = 0, right = len - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < nums[right]) { // nums[mid]在右半部分升序区间内
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] > nums[right]) { // nums[mid]在左半部分升序区间内
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // nums[mid] == nums[right]
+                right--;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 82. 删除排序链表中的重复元素 II
+     * 给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+     * <p>
+     * 示例 1:
+     * 输入: 1->2->3->3->4->4->5
+     * 输出: 1->2->5
+     * <p>
+     * 示例 2:
+     * 输入: 1->1->1->2->3
+     * 输出: 2->3
+     */
+    public ListNode deleteDuplicates1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        if (head.val == head.next.val) {
+            while (head != null && head.next != null && head.val == head.next.val) {
+                head = head.next;
+            }
+            return deleteDuplicates(head.next);
+        } else {
+            head.next = deleteDuplicates(head.next);
+            return head;
+        }
+    }
 
     /**
      * 83. 删除排序链表中的重复元素
@@ -56,6 +127,187 @@ public class T81_90 {
             maxArea = Math.max(maxArea, heights[i] * (len - i));
         }
         return maxArea;
+    }
+
+    /**
+     * 85. 最大矩形
+     * 给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入:
+     * [
+     * ["1","0","1","0","0"],
+     * ["1","0","1","1","1"],
+     * ["1","1","1","1","1"],
+     * ["1","0","0","1","0"]
+     * ]
+     * 输出: 6
+     *
+     * @param matrix
+     * @return
+     */
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+        int col = matrix[0].length;
+        int[] height = new int[col];
+        int[] left = new int[col];
+        int[] right = new int[col];
+        Arrays.fill(left, -1);
+        Arrays.fill(right, col);
+        int max = 0;
+
+        for (char[] m : matrix) {
+            for (int j=0; j<col; j++) {
+                if (m[j] == '0') height[j] = 0;
+                else height[j]++;
+            }
+
+            int b = -1;
+            for (int j=0; j<col; j++) {
+                if (m[j] == '0') {
+                    left[j] = -1;
+                    b = j;
+                } else {
+                    left[j] = Math.max(left[j], b);
+                }
+            }
+
+            b = col;
+            for (int j=col-1; j>=0; j--) {
+                if (m[j] == '0') {
+                    right[j] = col;
+                    b = j;
+                } else {
+                    right[j] = Math.min(right[j], b);
+                }
+            }
+
+            for (int j=col-1; j>=0; j--) {
+                int area = (right[j] - left[j] - 1) * height[j];
+                max = Math.max(max, area);
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 86. 分隔链表
+     * 给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+     * 你应当保留两个分区中每个节点的初始相对位置。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: head = 1->4->3->2->5->2, x = 3
+     * 输出: 1->2->2->4->3->5
+     */
+    public ListNode partition(ListNode head, int x) {
+        ListNode before_head = new ListNode(0);
+        ListNode before = before_head;
+        ListNode after_head = new ListNode(0);
+        ListNode after = after_head;
+
+        while (head != null) {
+
+            // If the original list node is lesser than the given x,
+            // assign it to the before list.
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                // If the original list node is greater or equal to the given x,
+                // assign it to the after list.
+                after.next = head;
+                after = after.next;
+            }
+
+            // move ahead in the original list
+            head = head.next;
+        }
+
+        // Last node of "after" list would also be ending node of the reformed list
+        after.next = null;
+
+        // Once all the nodes are correctly assigned to the two lists,
+        // combine them to form a single list which would be returned.
+        before.next = after_head.next;
+
+        return before_head.next;
+    }
+
+    /**
+     * 87. 扰乱字符串
+     * 给定一个字符串 s1，我们可以把它递归地分割成两个非空子字符串，从而将其表示为二叉树。
+     * 下图是字符串 s1 = "great" 的一种可能的表示形式。
+     * <p>
+     * great
+     * /    \
+     * gr    eat
+     * / \    /  \
+     * g   r  e   at
+     * / \
+     * a   t
+     * 在扰乱这个字符串的过程中，我们可以挑选任何一个非叶节点，然后交换它的两个子节点。
+     * 例如，如果我们挑选非叶节点 "gr" ，交换它的两个子节点，将会产生扰乱字符串 "rgeat" 。
+     * <p>
+     * rgeat
+     * /    \
+     * rg    eat
+     * / \    /  \
+     * r   g  e   at
+     * / \
+     * a   t
+     * 我们将 "rgeat” 称作 "great" 的一个扰乱字符串。
+     * 同样地，如果我们继续交换节点 "eat" 和 "at" 的子节点，将会产生另一个新的扰乱字符串 "rgtae" 。
+     * <p>
+     * rgtae
+     * /    \
+     * rg    tae
+     * / \    /  \
+     * r   g  ta  e
+     * / \
+     * t   a
+     * 我们将 "rgtae” 称作 "great" 的一个扰乱字符串。
+     * 给出两个长度相等的字符串 s1 和 s2，判断 s2 是否是 s1 的扰乱字符串。
+     * <p>
+     * 示例 1:
+     * 输入: s1 = "great", s2 = "rgeat"
+     * 输出: true
+     * <p>
+     * 示例 2:
+     * 输入: s1 = "abcde", s2 = "caebd"
+     * 输出: false
+     */
+    public boolean isScramble(String s1, String s2) {
+        if (s1.length() == 0 && s2.length() == 0) return true;
+        if (s1.length() != s2.length()) return false;
+        return dfs(s1.toCharArray(), s2.toCharArray(), 0, 0, s1.length());
+    }
+
+    private boolean dfs(char[] s1, char[] s2, int start1, int start2, int len) {
+        if (len == 1) {
+            return s1[start1] == s2[start2];
+        }
+        if (!equals(s1, s2, start1, start2, len)) {
+            return false;
+        }
+        for (int i = 1; i < len; i++) {
+            if (dfs(s1, s2, start1, start2, i) && dfs(s1, s2, start1 + i, start2 + i, len - i)) return true;
+            if (dfs(s1, s2, start1, start2 + len - i, i) && dfs(s1, s2, start1 + i, start2, len - i)) return true;
+        }
+        return false;
+    }
+
+    private boolean equals(char[] s1, char[] s2, int start1, int start2, int len) {
+        int[] charArr = new int[26];
+        for (int i = 0; i < len; i++) {
+            charArr[s1[start1 + i] - 'a']++;
+            charArr[s2[start2 + i] - 'a']--;
+        }
+        for (int item : charArr) {
+            if (item != 0) return false;
+        }
+        return true;
     }
 
     /**
@@ -157,22 +409,22 @@ public class T81_90 {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         res.add(new ArrayList<>());
-        if(nums == null || nums.length == 0) return res;
+        if (nums == null || nums.length == 0) return res;
         Arrays.sort(nums);
         List<Integer> tmp = new ArrayList<>();
         tmp.add(nums[0]);
         res.add(tmp);
-        if(nums.length == 1) return res;
+        if (nums.length == 1) return res;
 
         int lastLen = 1;
 
-        for(int i = 1; i < nums.length; i++){
+        for (int i = 1; i < nums.length; i++) {
             int size = res.size();
-            if(nums[i] != nums[i-1]){
+            if (nums[i] != nums[i - 1]) {
                 lastLen = size;
             }
 
-            for(int j = size - lastLen; j < size; j++){
+            for (int j = size - lastLen; j < size; j++) {
                 List<Integer> inner = new ArrayList(res.get(j));
                 inner.add(nums[i]);
                 res.add(inner);
--- src/test/java/com/luo/algorithm/_1_100/Test81_90.java
+++ src/test/java/com/luo/algorithm/_1_100/Test81_90.java
@@ -8,6 +8,31 @@ public class Test81_90 {
     T81_90 t = new T81_90();
 
     @Test
+    public void test81() {
+        System.out.println(t.search(new int[]{2, 5, 6, 0, 0, 1, 2}, 0));
+        System.out.println(t.search(new int[]{2, 5, 6, 0, 0, 1, 2}, 3));
+    }
+
+    @Test
+    public void test82() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next.next = new ListNode(5);
+        System.out.println(t.deleteDuplicates1(head));
+
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(1);
+        head2.next.next = new ListNode(1);
+        head2.next.next.next = new ListNode(2);
+        head2.next.next.next.next = new ListNode(3);
+        System.out.println(t.deleteDuplicates1(head2));
+    }
+
+    @Test
     public void test83() {
         ListNode head = new ListNode(1);
         head.next = new ListNode(1);
@@ -26,6 +51,29 @@ public class Test81_90 {
     public void test84() {
         System.out.println(t.largestRectangleArea(new int[]{2, 1, 5, 6, 2, 3}));
         System.out.println(t.largestRectangleArea(new int[]{2, 1, 7, 6, 2, 3}));
+    }
+
+    @Test
+    public void test85() {
+        char[][] m = {{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
+        System.out.println(t.maximalRectangle(m));
+    }
+
+    @Test
+    public void test86() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(2);
+        System.out.println(t.partition(head, 3));
+    }
+
+    @Test
+    public void test87() {
+        System.out.println(t.isScramble("great", "rgeat"));
+        System.out.println(t.isScramble("abcde", "caebd"));
     }
 
     @Test
