ecae552894f07e46df8780503a0ec2356d8aaea9
罗远新 <402135175@qq.com>
2020-09-26 09:47:42 +0800

    151,153,154,160

--- src/main/java/com/luo/algorithm/_101_200/T151_160.java
+++ src/main/java/com/luo/algorithm/_101_200/T151_160.java
@@ -4,6 +4,35 @@ import java.util.Stack;
 
 public class T151_160 {
     /**
+     * 151. 翻转字符串里的单词
+     * 给定一个字符串，逐个翻转字符串中的每个单词。
+     * <p>
+     * 示例 1：
+     * 输入: "the sky is blue"
+     * 输出: "blue is sky the"
+     * <p>
+     * 示例 2：
+     * 输入: "  hello world!  "
+     * 输出: "world! hello"
+     * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+     * <p>
+     * 示例 3：
+     * 输入: "a good   example"
+     * 输出: "example good a"
+     * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+     */
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] words = s.split(" ");
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (words[i].length() > 0 && i != 0)
+                sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+    /**
      * 152. 乘积最大子数组
      * 给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
      * <p>
@@ -33,6 +62,77 @@ public class T151_160 {
             max = Math.max(max, iMax);
         }
         return max;
+    }
+
+    /**
+     * 153. 寻找旋转排序数组中的最小值
+     * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+     * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+     * 请找出其中最小的元素。
+     * 你可以假设数组中不存在重复元素。
+     * <p>
+     * 示例 1:
+     * 输入: [3,4,5,1,2]
+     * 输出: 1
+     * <p>
+     * 示例 2:
+     * 输入: [4,5,6,7,0,1,2]
+     * 输出: 0
+     */
+    public int findMin153(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int left = 0, right = nums.length - 1;
+        if (nums[right] > nums[0]) {
+            return nums[0];
+        }
+        while (right >= left) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+            if (nums[mid - 1] > nums[mid]) {
+                return nums[mid];
+            }
+            if (nums[mid] > nums[0]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 154. 寻找旋转排序数组中的最小值 II
+     * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+     * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+     * 请找出其中最小的元素。
+     * 注意数组中可能存在重复的元素。
+     * <p>
+     * 示例 1：
+     * 输入: [1,3,5]
+     * 输出: 1
+     * <p>
+     * 示例 2：
+     * 输入: [2,2,2,0,1]
+     * 输出: 0
+     */
+    public int findMin154(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] < nums[high]) {
+                high = pivot;
+            } else if (nums[pivot] > nums[high]) {
+                low = pivot + 1;
+            } else {
+                high -= 1;
+            }
+        }
+        return nums[low];
     }
 
     /**
@@ -95,5 +195,40 @@ public class T151_160 {
         public int getMin() {
             return min;
         }
+    }
+
+    /**
+     * 160. 相交链表
+     * 编写一个程序，找到两个单链表相交的起始节点。
+     * 如下面的两个链表：
+     *
+     * 在节点 c1 开始相交。
+     *
+     * 示例 1：
+     * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+     * 输出：Reference of the node with value = 8
+     * 输入解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+     * 从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+     *
+     *
+     * 示例 2：
+     * 输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+     * 输出：Reference of the node with value = 2
+     * 输入解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。
+     * 从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+     *
+     * 示例 3：
+     * 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+     * 输出：null
+     * 输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+     * 解释：这两个链表不相交，因此返回 null。
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode ha = headA, hb = headB;
+        while (ha != hb) {
+            ha = ha != null ? ha.next : headB;
+            hb = hb != null ? hb.next : headA;
+        }
+        return ha;
     }
 }
--- src/test/java/com/luo/algorithm/_101_200/Test151_160.java
+++ src/test/java/com/luo/algorithm/_101_200/Test151_160.java
@@ -7,15 +7,35 @@ public class Test151_160 {
     T151_160 t = new T151_160();
 
     @Test
+    public void test151() {
+        System.out.println(t.reverseWords("the sky is blue"));
+        System.out.println(t.reverseWords("  hello world!  "));
+        System.out.println(t.reverseWords("a good   example"));
+    }
+
+    @Test
     public void test152() {
         int[] nums1 = {2, 3, -2, 4};
         int[] nums2 = {-2, 0, -1};
         System.out.println(t.maxProduct(nums1));
         System.out.println(t.maxProduct(nums2));
     }
+
+    @Test
+    public void test153() {
+        System.out.println(t.findMin153(new int[]{3, 4, 5, 1, 2}));
+        System.out.println(t.findMin153(new int[]{4, 5, 6, 7, 0, 1, 2}));
+    }
+
+    @Test
+    public void test154() {
+        System.out.println(t.findMin154(new int[]{1, 3, 5}));
+        System.out.println(t.findMin154(new int[]{2, 2, 2, 0, 1}));
+    }
+
     @Test
     public void test155() {
-        int[][] testTable = {{1,-2,3},{2,7,9,-3,1},{7,10,4,3,1},{11,6,2,7}};
+        int[][] testTable = {{1, -2, 3}, {2, 7, 9, -3, 1}, {7, 10, 4, 3, 1}, {11, 6, 2, 7}};
         for (int[] ito : testTable) {
             test(ito);
         }
@@ -26,7 +46,7 @@ public class Test151_160 {
         int rtn;
         long begin = System.currentTimeMillis();
         for (int i = 0; i < ito.length; i++) {
-            System.out.print(ito[i]+" ");
+            System.out.print(ito[i] + " ");
             solution.push(ito[i]);
         }
         System.out.println();
@@ -35,15 +55,48 @@ public class Test151_160 {
         rtn = solution.top();//执行程序
         long end = System.currentTimeMillis();
 
-        System.out.println(": rtn=" +rtn);
+        System.out.println(": rtn=" + rtn);
 
 
         rtn = solution.getMin();//执行程序
 
-        System.out.println(": rtn=" +rtn);
+        System.out.println(": rtn=" + rtn);
 
         System.out.println();
         System.out.println("耗时：" + (end - begin) + "ms");
         System.out.println("-------------------");
+    }
+
+    @Test
+    public void test160() {
+        ListNode headA1 = new ListNode(4);
+        headA1.next = new ListNode(1);
+        headA1.next.next = new ListNode(8);
+        headA1.next.next.next = new ListNode(4);
+        headA1.next.next.next.next = new ListNode(5);
+        ListNode headB1 = new ListNode(5);
+        headB1.next = new ListNode(0);
+        headB1.next.next = new ListNode(1);
+        headB1.next.next.next = new ListNode(8);
+        headB1.next.next.next.next = new ListNode(4);
+        headB1.next.next.next.next.next = new ListNode(5);
+        System.out.println(t.getIntersectionNode(headA1, headB1));
+
+        ListNode headA2 = new ListNode(0);
+        headA2.next = new ListNode(9);
+        headA2.next.next = new ListNode(1);
+        headA2.next.next.next = new ListNode(5);
+        headA2.next.next.next.next = new ListNode(4);
+        ListNode headB2 = new ListNode(3);
+        headB2.next = new ListNode(2);
+        headB2.next.next = new ListNode(4);
+        System.out.println(t.getIntersectionNode(headA2, headB2));
+
+        ListNode headA3 = new ListNode(2);
+        headA3.next = new ListNode(6);
+        headA3.next.next = new ListNode(4);
+        ListNode headB3 = new ListNode(1);
+        headB3.next = new ListNode(5);
+        System.out.println(t.getIntersectionNode(headA3, headB3));
     }
 }
